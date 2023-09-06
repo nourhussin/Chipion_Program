14fd57ab0e707c6ff3a973861ac52e3cfc0b1ea1
Nour Hussin <n0ur0eldiin@gmail.com>
2023-09-06 16:30:01 +0300

    Add Reset Option

--- Phase1_DigitalDesign/SBqM_Project/counter_nbits.v
+++ Phase1_DigitalDesign/SBqM_Project/counter_nbits.v
@@ -1,33 +1,39 @@
 module counter_nbits #(parameter N = 3)(
-    input front_photocell, back_photocell, clk,
+    input front_photocell, back_photocell, clk, reset,
     output reg[N-1:0] Pcount
 );
 
     always @(posedge clk)
     begin
-        case({front_photocell,back_photocell})
+        if(reset)
+            Pcount <= 0;
+            
+        else
+        begin
+            case({front_photocell,back_photocell})
 
-        2'b11: // Person entered and person exited
-                Pcount <= Pcount;
-
-        2'b10: // Person entered
-                if(&Pcount) //max number 111
-                    //! Sensor Error
+            2'b11: // Person entered and person exited
                     Pcount <= Pcount;
-                else 
-                    Pcount <= Pcount +1;
 
-        2'b01: // Person exited
-                if (Pcount == 0) //min number 000
-                    //! Sensor Error
+            2'b10: // Person entered
+                    if(&Pcount) //max number 111
+                        //! Sensor Error
+                        Pcount <= Pcount;
+                    else 
+                        Pcount <= Pcount +1;
+
+            2'b01: // Person exited
+                    if (Pcount == 0) //min number 000
+                        //! Sensor Error
+                        Pcount <= Pcount;
+                    else
+                        Pcount <= Pcount -1;
+
+            2'b00: // Nothing happed
                     Pcount <= Pcount;
-                else
-                    Pcount <= Pcount -1;
 
-        2'b00: // Nothing happed
-                Pcount <= Pcount;
-
-        endcase
+            endcase
+        end
     end
 
 endmodule
--- Phase1_DigitalDesign/SBqM_Project/queue_status.v
+++ Phase1_DigitalDesign/SBqM_Project/queue_status.v
@@ -1,27 +1,36 @@
 module queue_status #(parameter N = 3)(
-    input clk,
+    input clk, reset,
     input[N-1:0] Pcount,
     output reg empty_flag, full_flag
 );
 
     always @(posedge clk)
     begin
-        if (&Pcount) // all ones ---> max value
-        begin //! Full Condition
-            empty_flag = 1'b0;
-            full_flag  = 1'b1;
+        if(reset)
+        begin
+            empty_flag <= 1'b1;
+            full_flag  <= 1'b0;
         end
 
-        else if(Pcount == 0) // min value
-        begin //! Empty Condition
-            empty_flag = 1'b1;
-            full_flag  = 1'b0;
-        end
+        else
+        begin
+            if (&Pcount) // all ones ---> max value
+            begin //! Full Condition
+                empty_flag = 1'b0;
+                full_flag  = 1'b1;
+            end
 
-        else 
-        begin //! Occupied Condition
-            empty_flag = 1'b0;
-            full_flag  = 1'b0;
+            else if(Pcount == 0) // min value
+            begin //! Empty Condition
+                empty_flag = 1'b1;
+                full_flag  = 1'b0;
+            end
+
+            else 
+            begin //! Occupied Condition
+                empty_flag = 1'b0;
+                full_flag  = 1'b0;
+            end
         end
     end
 
