a7eca8f2d47032f1854557138d9aa4bb8f543c79
Nour Hussin <n0ur0eldiin@gmail.com>
2023-10-01 12:21:16 +0300

    Add Program Counter

---
+++ Phase2_CA/Single_Cycle_MIPS/program_counter.v
@@ -0,0 +1,13 @@
+module program_coutner(
+    input clk, reset_n,
+    input[31:0] PC_bar,
+    output reg[31:0] PC
+);
+    always @(posedge clk, negedge reset_n)
+    begin
+        if(!reset_n)
+            PC <= 32'b0;
+        else
+            PC <= PC_bar;
+    end
+endmodule
