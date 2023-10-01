699ab370ef022c868efb56774db1fe55c99ee3a9
Nour Hussin <n0ur0eldiin@gmail.com>
2023-10-01 12:12:13 +0300

    Fix Write and Read Issues

--- Phase2_CA/Single_Cycle_MIPS/data_memory.v
+++ Phase2_CA/Single_Cycle_MIPS/data_memory.v
@@ -1,17 +1,17 @@
 module data_memory(
     input clk,WE,
     input[31:0] A, WD,
-    output[31:0] RD,
+    output reg[31:0] RD,
     output[15:0] test_value
 );
     reg[31:0] mem[255:0];
-    assign RD = mem[A];
     // Is that LSB?
     assign test_value = mem[0];
-
-    always @(posedge clk)
+    always @(posedge clk, negedge WE)
     begin
         if(WE)
             mem[A] <= WD;
+        else
+            RD <= mem[A];
     end
 endmodule
--- Phase2_CA/Single_Cycle_MIPS/register_file.v
+++ Phase2_CA/Single_Cycle_MIPS/register_file.v
@@ -2,16 +2,16 @@ module register_file(
     input clk, WE3,
     input[4:0] A1, A2, A3,
     input[31:0] WD3,
-    output[31:0] RD1, RD2
+    output reg[31:0] RD1, RD2
 );
     reg[31:0] mem[31:0];
-    assign RD1 = mem[A1];
-    assign RD2 = mem[A2];
-
-    always @(posedge clk)
+    always @(posedge clk, negedge WE3)
     begin
         if(WE3)
             mem[A3] <= WD3;
+        else
+            RD1 <= mem[A1];
+            RD2 <= mem[A2];
     end
 
 endmodule
